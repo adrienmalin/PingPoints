f804af8b9d889a6603b0805f5482de81465b362b
adrienmalin <41926238+adrienmalin@users.noreply.github.com>
2018-08-13 01:05:51 +0200

    ...

---
+++ app/src/main/java/adrienmalin/pingpoints/Dialog.kt
@@ -0,0 +1,26 @@
+package adrienmalin.pingpoints
+
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+
+
+class Dialog : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        // Use the Builder class for convenient dialog construction
+        val builder = AlertDialog.Builder(activity)
+        builder.setTitle(R.string.dialog_title)
+                .setMessage(R.string.dialog_message)
+                .setPositiveButton(R.string.go, DialogInterface.OnClickListener { dialog, id ->
+                    // FIRE ZE MISSILES!
+                })
+                .setNegativeButton(R.string.cancel, DialogInterface.OnClickListener { dialog, id ->
+                    // User cancelled the dialog
+                })
+                .setView(view)
+        // Create the AlertDialog object and return it
+        return builder.create()
+    }
+}
--- app/src/main/java/adrienmalin/pingpoints/MainActivity.kt
+++ app/src/main/java/adrienmalin/pingpoints/MainActivity.kt
@@ -3,6 +3,7 @@ package adrienmalin.pingpoints
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,10 +11,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    val joueurs: Array<Joueur> = arrayOf(Joueur("Joueur 1"), Joueur("Joueur 2"))
 
-    val serveur: Joueur? = null
+    val joueurs: Array<Joueur> = arrayOf(Joueur(getString(R.string.nom_joueur_1)), Joueur(getString(R.string.nom_joueur_2)))
+    val serveur: Int? = null
 
 }
 
-class Joueur(nom: String)
+class Joueur(nom: String, score: Int = 0)
