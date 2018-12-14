4bb1ff5b42da734467396f8dc0410a2a1a28b24b
adrienmalin <41926238+adrienmalin@users.noreply.github.com>
2018-12-14 20:46:27 +0100

    use string.equals(ignoreCase=true) instead of regex

--- app/src/main/java/adrienmalin/pingpoints/Player.kt
+++ app/src/main/java/adrienmalin/pingpoints/Player.kt
@@ -1,9 +1,6 @@
 package adrienmalin.pingpoints
 
-import java.util.regex.Pattern
-
 data class Player (
     var name: String,
-    var score: Int,
-    var pattern: Pattern? = null
+    var score: Int
 )
